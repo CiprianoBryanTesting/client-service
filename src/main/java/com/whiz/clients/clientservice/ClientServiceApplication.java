119f78ddef5d5983619aa965405e11371e655266
Bryan Tony <ciprianot.bryan@gmail.com>
2023-02-25 12:14:02 -0500

    Enable conexion to Naming-Server

--- src/main/java/com/whiz/clients/clientservice/ClientServiceApplication.java
+++ src/main/java/com/whiz/clients/clientservice/ClientServiceApplication.java
@@ -2,7 +2,11 @@ package com.whiz.clients.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.*;
+import org.springframework.cloud.openfeign.*;
 
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class ClientServiceApplication {
 
